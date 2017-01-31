72ab2479ff75a393cc197718193bf347a8eccaa6
Alexander Romanov <vssanya@yandex.ru>
2017-01-31 12:10:44 +0300

    Fix bugs in calculation of hartree potential

--- src/hartree_potential.c
+++ src/hartree_potential.c
@@ -1,15 +1,14 @@
 #include "hartree_potential.h"
 
-double F_first(double[2] F, int l, sphere_grid_t const* grid, double f[grid->n[iR]]) {
-	double const dr = grid->dr
+double F_first(double F[2], int l, sh_grid_t const* grid, double f[grid->n[iR]]) {
+    double const dr = grid->d[iR];
 
-	F[0] = 0;
-	F[1] = 0;
+    F[0] = 0.0;
+    F[1] = 0.0;
 
 	for (int ir = 0; ir < grid->n[iR]; ++ir) {
-		double const r = sp_grid_r(grid, ir);
+        double const r = sh_grid_r(grid, ir);
 		F[1] += f[ir]*pow(dr/r, l)/r;
-		r += dr;
 	}
 
 	return (F[0] + F[1])*dr;
@@ -19,9 +18,9 @@ double F_first(double[2] F, int l, sphere_grid_t const* grid, double f[grid->n[i
  * \brief \f[F_l(r, f) = \int dr' \frac{r_<^l}{r_>^{l+1}} f(r')\f]
  * \param[in,out] F
  * */
-double F_next(double[2] F, int l, int ir, sh_grid_t const* grid, double f[grid->n[iR]]) {
-	double const dr = grid->dr
-	double const r = ir*dr;
+double F_next(double F[2], int l, int ir, sh_grid_t const* grid, double f[grid->n[iR]]) {
+    double const dr = grid->d[iR];
+    double const r = ir*dr;
 	double const r_dr = r + dr;
 
 	F[0] = pow(r/r_dr, l+1)*(F[0] + f[ir]/r);
@@ -35,16 +34,17 @@ void hartree_potential_l0(ks_orbitals_t const* orbs, double U[orbs->grid->n[iR]]
 
 	double f[grid->n[iR]];
 	for (int ie = 0; ie < orbs->ne; ++ie) {
-		for (int il = 0; il < grid->n[iL]; ++il) {
+        sphere_wavefunc_t const* wf = orbs->wf[ie];
+        for (int il = 0; il < grid->n[iL]; ++il) {
 			for (int ir = 0; ir < grid->n[iR]; ++ir) {
-				f[ir] += swf_get_abs_2(orbs->wf[ie], ir, il)
+                f[ir] += swf_get_abs_2(wf, ir, il);
 			}
 		}
 	}
 
 	double F[2];
 	U[0] = 2*F_first(F, 0, orbs->grid, f);
-	for (int ir = 0; ir < grid->Nr; ++ir) {
+    for (int ir = 0; ir < grid->n[iR]; ++ir) {
 		U[ir] = 2*F_next(F, 0, ir, orbs->grid, f);
 	}
 }
@@ -54,19 +54,20 @@ void hartree_potential_l1(ks_orbitals_t const* orbs, double U[orbs->grid->n[iR]]
 
 	double f[grid->n[iR]];
 	for (int ie = 0; ie < orbs->ne; ++ie) {
+        sphere_wavefunc_t const* wf = orbs->wf[ie];
 		for (int il = 0; il < grid->n[iL]; ++il) {
 			for (int ir = 0; ir < grid->n[iR]; ++ir) {
 				f[ir] += swf_get(wf, ir, il) * (
 						clm(il-1, wf->m)*conj(swf_get(wf, ir, il-1)) +
 						clm(il  , wf->m)*conj(swf_get(wf, ir, il+1))
-						)
+                        );
 			}
 		}
 	}
 
 	double F[2];
 	U[0] = 2*F_first(F, 0, orbs->grid, f);
-	for (int ir = 0; ir < grid->Nr; ++ir) {
+	for (int ir = 0; ir < grid->n[iR]; ++ir) {
 		U[ir] = 2*F_next(F, 0, ir, orbs->grid, f);
 	}
 }
@@ -76,9 +77,10 @@ void hartree_potential_l2(ks_orbitals_t const* orbs, double U[orbs->grid->n[iR]]
 
 	double f[grid->n[iR]];
 	for (int ie = 0; ie < orbs->ne; ++ie) {
-		for (int il = 0; il < grid->n[iL]; ++il) {
+        sphere_wavefunc_t const* wf = orbs->wf[ie];
+        for (int il = 0; il < grid->n[iL]; ++il) {
 			for (int ir = 0; ir < grid->n[iR]; ++ir) {
-				swf_get(wf, ir, il) * (
+                f[ir] += swf_get(wf, ir, il) * (
 						plm(il, wf->m)*conj(swf_get(wf, ir, il)) +
 						qlm(il, wf->m)*conj(swf_get(wf, ir, il+2)) +
 						qlm(il-2, wf->m)*conj(swf_get(wf, ir, il-2))
@@ -89,7 +91,7 @@ void hartree_potential_l2(ks_orbitals_t const* orbs, double U[orbs->grid->n[iR]]
 
 	double F[2];
 	U[0] = 2*F_first(F, 0, orbs->grid, f);
-	for (int ir = 0; ir < grid->Nr; ++ir) {
+    for (int ir = 0; ir < grid->n[iR]; ++ir) {
 		U[ir] = 2*F_next(F, 0, ir, orbs->grid, f);
 	}
 }
