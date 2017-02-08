97c20c75e5d69bb6a549882313a248ee15646497
Alexander Romanov <vssanya@yandex.ru>
2017-02-08 15:53:36 +0300

    Tets calc jrcd(phase)

--- jrcd.py
+++ jrcd.py
@@ -1,34 +1,53 @@
-import time
+from mpi4py import MPI
 
 import numpy as np
-import matplotlib.pyplot as plt
+from tdse import grid, wavefunc, field, workspace, hydrogen, calc, utils
 
-from tdse import grid, wavefunc, field, workspace, hydrogen, calc
+def calc_jrcd(I0, length, t_fwhm, alpha, phase):
+    freq = utils.length_to_freq(800, 'nm')
+    E0 = utils.I_to_E(I0)
+    tp = utils.t_fwhm(t_fwhm, 'fs')
+    t0 = utils.t_shift(tp, I0, Imin=1e10)
 
+    field = field.TwoColorPulseField(
+        E0 = E0,
+        alpha = alpha,
+        freq = freq,
+        phase = phase,
+        tp = tp,
+        t0 = t0
+    )
 
-freq = 5.6e-2
-E0 = 3.77e-2
-T = 2.0*np.pi/freq
-tp = T*2.05/2.67/(2*np.log(2))
-dt = 0.025
-Nt = 3*T/dt
+    dt = 0.025
+    dr = 0.1
+    Nt = (2*t0)/dt
 
-t = np.linspace(0, 3*T, Nt)
+    r_max = utils.r_max(E, alpha, freq)
 
-field = field.TwoColorPulseField(
-    E0 = E0,
-    alpha = 0.0,
-    freq = freq,
-    phase = 0.0,
-    tp = tp,
-    t0 = 1.5*T
-)
+    grid = grid.SGrid(Nr=r_max/dr, Nl=40, r_max=r_max)
+    wf = hydrogen.ground_state(grid)
+    ws = workspace.SKnWorkspace(dt=dt, grid=grid)
 
-grid = grid.SGrid(Nr=2000, Nl=40, r_max=250)
-wf = hydrogen.ground_state(grid)
-ws = workspace.SKnWorkspace(dt=dt, grid=grid)
+    return calc.jrcd_t(ws, wf, field, Nt)
 
-start_time = time.time()
-jrcd = calc.jrcd_t(ws, wf, field, Nt)
-print("Time of execution: {}".format(time.time() - start_time))
-print(jrcd)
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+if rank == 0:
+    phase = np.linspace(0.0, 2*np.pi, 20)
+
+phase = comm.scatter(phase, root=0)
+
+jrcd = calc_jrcd(
+        I0=1e14,
+        length=800,
+        t_fwhm=50,
+        alpha=0.2,
+        phase=phase
+        )
+
+jrcd = comm.gather(jrcd, root=0)
+
+if rank == 0:
+    np.savetxt('res.txt', [phase, jrcd])
---
+++ tdse/utils.py
@@ -0,0 +1,40 @@
+import numpy as np
+from . import const
+
+UNIT = {
+        'au': 1.0, # atomic unit
+
+        's' : 1 / 2.418884326505e-17,
+        'fs': 1 / 2.418884326505e-2,
+
+        'm': 1 / 5.2917721092e-11,
+        'nm': 1 / 5.2917721092e-2,
+}
+
+def unit_to(value, u_from='au', u_to='au'):
+    return UNIT[u_from]*value/UNIT[u_to]
+
+def t_fwhm(fwhm, u='fs', u_to='au'):
+    return unit_to(fwhm, u, u_to)*2*np.log(2)
+
+def length_to_freq(length, u='nm', u_to='au'):
+    freq_au = 2*np.pi*const.C / unit_to(length, u)
+    return unit_to(freq_au, u_to=u_to)
+
+def t_shift(tp, I0, Imin):
+    return np.sqrt(0.5*tp**2*np.log(I0/Imin))
+
+def r_max(E, alpha, freq):
+    return 3*E/freq**2*(1 + alpha/4)
+
+"""
+    I (W/cm^2)
+    E (au)
+"""
+def I_to_E(I):
+    a = 1e14 / (5.34e-2)**2
+    return np.sqrt(I/a)
+
+def E_to_I(E):
+    a = 1e14 / (5.34e-2)**2
+    return a * E**2
