5854f7f6391f40ced837360ea0bc565725ed8f7d
Alexander Romanov <vssanya@yandex.ru>
2018-10-15 16:23:35 +0300

    Add new plot functions

--- tdse/plot.py
+++ tdse/plot.py
@@ -1,8 +1,25 @@
 import numpy as np
+
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 import tdse.utils
 
+def init_style_article():
+    from matplotlib.backends.backend_pgf import FigureCanvasPgf
+    mpl.backend_bases.register_backend('pdf', FigureCanvasPgf)
+
+    plt.style.use('article')
+
+def figsize(scale, aspect=1, width_pt=469.0/2):
+    """
+    width_pt - get this from LaTeX using \\the\\textwidth
+    """
+    inches_per_pt = 1.0/72.27                # Convert pt to inch
+    fig_width = width_pt*inches_per_pt*scale # width in inches
+    fig_height = fig_width*aspect            # height in inches
+    fig_size = [fig_width,fig_height]
+    return fig_size
 
 def spectral_density(dt, freq, Sw, Nmax=100, **kwargs):
     N = np.linspace(0, np.pi/dt, Sw.size) / freq
